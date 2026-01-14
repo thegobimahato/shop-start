95fc18fb4bf9f5fb0eb2f854967703396643f85b
Gobi Mahato <thegobimahato@gmail.com>
2026-01-14 09:34:28 +0530

    Initial Setup with shadcn ui

---
+++ src/router.tsx
@@ -0,0 +1,28 @@
+import { createRouter } from "@tanstack/react-router";
+import { QueryClient } from "@tanstack/react-query";
+import { setupRouterSsrQueryIntegration } from "@tanstack/react-router-ssr-query";
+
+// Import the generated route tree
+import { routeTree } from "./routeTree.gen";
+
+// Create a new router instance
+export const getRouter = () => {
+  const queryClient = new QueryClient();
+
+  const router = createRouter({
+    routeTree,
+    context: {
+      queryClient,
+    },
+
+    defaultPreload: "intent",
+    // defaultErrorComponent: DefaulErrorComponent,
+    // defaultNotFoundComponent: DefaultNotFoundComponent,
+    scrollRestoration: true,
+    defaultPreloadStaleTime: 0,
+  });
+
+  setupRouterSsrQueryIntegration({ router, queryClient });
+
+  return router;
+};
---
+++ src/routes/__root.tsx
@@ -0,0 +1,63 @@
+import { TanStackDevtools } from "@tanstack/react-devtools";
+import {
+  HeadContent,
+  Scripts,
+  createRootRouteWithContext,
+} from "@tanstack/react-router";
+import { TanStackRouterDevtoolsPanel } from "@tanstack/react-router-devtools";
+
+
+import type { QueryClient } from "@tanstack/react-query";
+import appCss from "../styles.css?url";
+
+export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
+  {
+    head: () => ({
+      meta: [
+        {
+          charSet: "utf-8",
+        },
+        {
+          name: "viewport",
+          content: "width=device-width, initial-scale=1",
+        },
+        {
+          title: "ShopStart — Build Multi-Tenant Stores Faster",
+        },
+      ],
+      links: [
+        {
+          rel: "stylesheet",
+          href: appCss,
+        },
+      ],
+    }),
+
+    shellComponent: RootDocument,
+  }
+);
+
+function RootDocument({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <head>
+        <HeadContent />
+      </head>
+      <body>
+        {children}
+        <TanStackDevtools
+          config={{
+            position: "bottom-right",
+          }}
+          plugins={[
+            {
+              name: "Tanstack Router",
+              render: <TanStackRouterDevtoolsPanel />,
+            },
+          ]}
+        />
+        <Scripts />
+      </body>
+    </html>
+  );
+}
